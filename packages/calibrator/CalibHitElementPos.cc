dbf3cf3640615dde6734f1cad51868b97d91e666
Patrick McCormack <wmccorma@submit00.mit.edu>
2023-12-04 16:19:40 -0500

    updating hit element calibrator

--- packages/calibrator/CalibHitElementPos.cc
+++ packages/calibrator/CalibHitElementPos.cc
@@ -5,13 +5,18 @@
 #include <phool/PHIODataNode.h>
 #include <phool/getClass.h>
 #include <geom_svc/GeomSvc.h>
+
+#include <ktracker/SRawEvent.h>
+
 #include "CalibHitElementPos.h"
+
 using namespace std;
 
 CalibHitElementPos::CalibHitElementPos(const std::string& name)
   : SubsysReco(name)
   , m_vec_hit   (0)
   , m_vec_trhit (0)
+  , _input_type(CalibHitElementPos::E1039)
 {
   ;
 }
@@ -28,23 +33,42 @@ int CalibHitElementPos::Init(PHCompositeNode* topNode)
 
 int CalibHitElementPos::InitRun(PHCompositeNode* topNode)
 {
-  m_vec_hit   = findNode::getClass<SQHitVector>(topNode, "SQHitVector");
-  m_vec_trhit = findNode::getClass<SQHitVector>(topNode, "SQTriggerHitVector");
-  if (!m_vec_hit || !m_vec_trhit) return Fun4AllReturnCodes::ABORTEVENT;
+  if(_input_type == CalibHitElementPos::E1039){
+    m_vec_hit   = findNode::getClass<SQHitVector>(topNode, "SQHitVector");
+    m_vec_trhit = findNode::getClass<SQHitVector>(topNode, "SQTriggerHitVector");
+    if (!m_vec_hit || !m_vec_trhit) return Fun4AllReturnCodes::ABORTEVENT;
+  }
+  else{
+    _rawEvent = findNode::getClass<SRawEvent>(topNode, "SRawEvent");
+  }
+  
   return Fun4AllReturnCodes::EVENT_OK;
 }
 
 int CalibHitElementPos::process_event(PHCompositeNode* topNode)
 {
   GeomSvc* geom = GeomSvc::instance();
-  for (SQHitVector::Iter it = m_vec_hit->begin(); it != m_vec_hit->end(); it++) {
-    SQHit* hit = *it;
-    hit->set_pos(geom->getMeasurement(hit->get_detector_id(), hit->get_element_id()));
+  if(_input_type == CalibHitElementPos::E1039){
+    for (SQHitVector::Iter it = m_vec_hit->begin(); it != m_vec_hit->end(); it++) {
+      SQHit* hit = *it;
+      hit->set_pos(geom->getMeasurement(hit->get_detector_id(), hit->get_element_id()));
+    }
+    for (SQHitVector::Iter it = m_vec_trhit->begin(); it != m_vec_trhit->end(); it++) {
+      SQHit* hit = *it;
+      hit->set_pos(geom->getMeasurement(hit->get_detector_id(), hit->get_element_id()));
+    }
   }
-  for (SQHitVector::Iter it = m_vec_trhit->begin(); it != m_vec_trhit->end(); it++) {
-    SQHit* hit = *it;
-    hit->set_pos(geom->getMeasurement(hit->get_detector_id(), hit->get_element_id()));
+  else{
+    std::vector<Hit>& hitAll = _rawEvent->getAllHits();
+    for(std::vector<Hit>::iterator iter = hitAll.begin(); iter != hitAll.end(); ++iter){
+      iter->setPos( geom->getMeasurement(iter->detectorID, iter->elementID) );
+    }
+    std::vector<Hit>& trhitAll = _rawEvent->getTriggerHits();
+    for(std::vector<Hit>::iterator iter = trhitAll.begin(); iter != trhitAll.end(); ++iter){
+      iter->setPos( geom->getMeasurement(iter->detectorID, iter->elementID) );
+    }
   }
+  
   return Fun4AllReturnCodes::EVENT_OK;
 }
 
--- packages/calibrator/CalibHitElementPos.h
+++ packages/calibrator/CalibHitElementPos.h
@@ -1,20 +1,32 @@
 #ifndef __CALIB_HIT_ELEMENT_POS_H__
 #define __CALIB_HIT_ELEMENT_POS_H__
 #include <fun4all/SubsysReco.h>
+
 class SQHitVector;
+
+class SRawEvent;
 
 /// SubsysReco module to set the position of SQHit using GeomSvc.
 class CalibHitElementPos: public SubsysReco {
   SQHitVector* m_vec_hit;
   SQHitVector* m_vec_trhit;
-
+  SRawEvent* _rawEvent;
+  
  public:
+  enum INPUT_TYPE  {E906, E1039};
+  
   CalibHitElementPos(const std::string &name = "CalibHitElementPos");
   virtual ~CalibHitElementPos();
+  
+  void setInputTy(CalibHitElementPos::INPUT_TYPE input_ty) { _input_type = input_ty; }
+  
   int Init(PHCompositeNode *topNode);
   int InitRun(PHCompositeNode *topNode);
   int process_event(PHCompositeNode *topNode);
   int End(PHCompositeNode *topNode);
+
+ protected:
+  CalibHitElementPos::INPUT_TYPE  _input_type;
 };
 
 #endif // __CALIB_HIT_ELEMENT_POS_H__
